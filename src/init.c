e2ece71f6646234908ea7aada8846144bf91b217
SeanBurford <SeanBurford@users.noreply.github.com>
2015-09-23 18:59:19 +1000

    Enable blinky_cpu to run from SRAM as well as flash.

--- src/init.c
+++ src/init.c
@@ -1,14 +1,7 @@
-// Initialization and interrupt vectors for the infineon 
-// XMC1100 microcontroller.
-// No claims are made as to the suitability of this code for any 
-// purpose whatsoever.  Use at your own risk!
-// Does not include initialization for C++ global/static objects
-// so probably best to use this with C projects for now.
-// Latest version of this code can be found by visiting
-// http://eleceng.dit.ie/frank and follow the links
-// Author: Frank Duignan
+/* Initialization and interrupt vectors for the infineon XMC1100 microcontroller
+   Based on work by Frank Duignan, modified by Sean Burford.
+   http://eleceng.dit.ie/frank/arm/BareMetalXMC2Go/index.html */
 
-static void init(void);
 extern int main();
 
 // The following are 'declared' in the linker script
@@ -18,26 +11,25 @@ extern unsigned int INIT_DATA_SIZE;
 extern unsigned int BSS_START;
 extern unsigned int BSS_SIZE;
 
-// The XMC1100 remaps interrupt vectors to RAM.  The boot loader
-// looks for the program entry address at 0x10001004 and the
-// initial stack pointer as 0x10001000.  These are defined in
-// the "vectors" section below.  The section "vectors" is 
-// placed at the beginning of flash by the linker script
-// Another useful feature of this chip is that you can 
-// pre-program startup considtions such as CPU speed and 
-// which periperals.  This is done by writing values in to
-// CLK_VAL1 and CLK_VAL2 below
-const void *FlashVectors[] __attribute__((section(".vectors"))) ={
-	(void *)0x20004000,	/* Top of stack  0x10001000 */ 
-	init,			/* Reset Handler 0x10001004 */
-	(void *)0	,	/* 0x10001008 */
-	(void *)0,		/* 0x1000100c */
-	(void *)0xffffffff,	/* CLK_VAL1   0x10001010 */
-	(void *)0xffffffff	/* CLK_VAL2   0x10001014 */
-};
+static void init()
+{
+	// do global/static data initialization
+	// This is will also copy the jump table for remapped IRQ vectors
+	// to RAM.
+	const unsigned int *src = &INIT_DATA_VALUES;
+	unsigned int *dest = &INIT_DATA_START;
+	int len;
+	for(len=(int)&INIT_DATA_SIZE / 4; len >= 0; len--)
+		dest[len] = src[len];
 
-// Weak interrupt handler definitions.  Declaring another function of the
-// same name will override the default unhandledIRQ handler for that interrupt.
+	// zero out the uninitialized global/static variables
+	unsigned int *const bss = &BSS_START;
+	for(len=(int)&BSS_SIZE / 4; len >= 0; len--)
+		bss[len] = 0;
+
+	main();
+}
+
 void __attribute__((interrupt("IRQ"))) unhandledIRQ(void) {
 	// IPSR holds the ISR number.
 	// Special return addresses:
@@ -54,6 +46,8 @@ void __attribute__((interrupt("IRQ"))) hardfaultDefault(void) {
 	}
 }
 
+// Weak interrupt handler definitions.  Declaring another function of the
+// same name will override the default unhandledIRQ handler for that interrupt.
 void __attribute__((weak, alias("hardfaultDefault"))) hardfaultHandler(void);
 void __attribute__((weak, alias("unhandledIRQ"))) systickHandler(void);
 void __attribute__((weak, alias("unhandledIRQ"))) SCU_SR0(void);
@@ -90,15 +84,13 @@ void __attribute__((weak, alias("unhandledIRQ"))) IRQ30(void);
 void __attribute__((weak, alias("unhandledIRQ"))) IRQ31(void);
 
 // The remaining interrupt vectors are relocated to RAM where a jump
-// table should be placed to the actual interrupt handlers.  The jump 
+// table should be placed to the actual interrupt handlers.  The jump
 // table takes the following form:
 //	ldr R0,=handler_address
 //  mov PC,R0
-// This pair of instructions fits into 4 bytes which
-// fits neatly into a jump table entry.
-inline void JumpTable(void) __attribute__(( section(".remapped_vectors")));
-inline void JumpTable(void) 
-{
+// This pair of instructions fits into 4 bytes which fits neatly into
+// a jump table entry.
+inline void __attribute__(( section(".interrupt_vectors"))) JumpTable(void) {
 	asm(" .long 0 "); // -15 Power up and warm reset
 	asm(" .long 0 "); // -14 NMI
 	asm(" ldr R0,=(hardfaultHandler) "); // -13 HardFault handler
@@ -111,8 +103,8 @@ inline void JumpTable(void)
 	asm(" .long 0 "); // -7 reserved
 	asm(" .long 0 "); // -6 reserved
 	asm(" .long 0 "); // -5 SVCall
-	asm(" .long 0 "); // -4 Debug monitor 
-	asm(" .long 0 "); // -3 reserved 
+	asm(" .long 0 "); // -4 Debug monitor
+	asm(" .long 0 "); // -3 reserved
 	asm(" .long 0 "); // -2 SVCall
 	asm(" ldr R0,=(systickHandler) "); // -1 Systick handler
 	asm(" mov PC,R0 ");
@@ -182,22 +174,22 @@ inline void JumpTable(void)
 	asm(" mov PC,R0 ");
 };
 
-static void init()
-{
-	// do global/static data initialization
-	// This is will also copy the jump table for remapped IRQ vectors
-	// to RAM.
-	const unsigned int *src = &INIT_DATA_VALUES;
-	unsigned int *dest = &INIT_DATA_START;
-	int len;
-	for(len=(int)&INIT_DATA_SIZE / 4; len >= 0; len--)
-		dest[len] = src[len];
+// The XMC1100 remaps interrupt vectors to RAM.  The boot loader
+// looks for the program entry address at 0x10001004 and the
+// initial stack pointer as 0x10001000.  These are defined in
+// the "vectors" section below.  The section "vectors" is
+// placed at the beginning of flash by the linker script
+// Another useful feature of this chip is that you can
+// pre-program startup considtions such as CPU speed and
+// which periperals.  This is done by writing values in to
+// CLK_VAL1 and CLK_VAL2 below
+const void *FlashVectors[] __attribute__((section(".vectors"))) ={
+	(void *)0x20004000,	/* Top of stack  0x10001000 */
+	init,			/* Reset Handler 0x10001004 */
+	(void *)0	,	/* 0x10001008 */
+	(void *)0,		/* 0x1000100c */
+	(void *)0xffffffff,	/* CLK_VAL1   0x10001010 */
+	(void *)0xffffffff	/* CLK_VAL2   0x10001014 */
+};
 
-	// zero out the uninitialized global/static variables
-	unsigned int *const bss = &BSS_START;
-	for(len=(int)&BSS_SIZE / 4; len >= 0; len--)
-		bss[len] = 0;
-
-	main();
-}
 
